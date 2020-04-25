69d018f0faf1abbf4255f77e7f3ad3f9d148c585
wazabicloud <wazabicloud@gmail.com>
2020-04-25 23:27:25 +0200

    sidebar hide

--- scripts.js
+++ scripts.js
@@ -2,3 +2,7 @@ function pidineria () {
     document.getElementById("vuoto").innerHTML = "BUONGIONNO PICCIMIMI! ❤"
     document.getElementById("vuoto").style.borderWidth = "4px";
 }
+
+function aprichiudibarra() {
+    document.getElementById("sidebar").style.width = "0";
+}
