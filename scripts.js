527052b779fc0df06de33cfc6228d9b58cf7befc
wazabicloud <wazabicloud@gmail.com>
2020-05-01 22:54:00 +0200

    link argomenti

--- scripts.js
+++ scripts.js
@@ -4,14 +4,7 @@ function pidineria () {
     document.getElementById("vuoto").style.padding = "15px";
     document.getElementById("premibtn").style.display = "none";
 }
-var sidestate = 0;
 
-function aprichiudibarra() {
-    if (sidestate == 0) {
-        document.getElementById("sidebar").style.width = "50vw";
-        sidestate = 1;
-    } else {
-        document.getElementById("sidebar").style.width = "0";
-        sidestate = 0;
-    };
-};
+function dropmenu() {
+    document.getElementById("drop-content").classList.toggle("show-content");
+}
