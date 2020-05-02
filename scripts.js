e6c820c77f6c66f32965526001201919bfe697f6
wazabicloud <wazabicloud@gmail.com>
2020-05-02 19:19:20 +0200

    jquery sidebar

--- scripts.js
+++ scripts.js
@@ -6,7 +6,20 @@ function pidineria () {
 };
 
 $(document).ready(function(){
+
+    //Dropdown buttons
+
     $(".dropbtn").click( function () {
         $(this).parent().next().slideToggle("slow");
+    });
+
+    //Sidebar sliding
+
+    $(".sidebarbtn").click( function () {
+        if ($(".sidebar").css("left") !== "0px") {
+            $(".sidebar").animate({left: '0'},500);
+        } else {
+            $(".sidebar").animate({left: '-50%'},500);
+        }
     });
 });
